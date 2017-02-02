c480beac3aa40d548197bf9843909372d948d757
Drew Parker <drew.i.parker@gmail.com>
2017-02-02 16:05:54 -0500

    Init commit

---
+++ routes/index.js
@@ -0,0 +1,41 @@
+var express = require('express');
+var router = express.Router();
+
+var students = [
+    "Sean",
+    "Drew",
+    "Daniel",
+    "Kyle",
+    "Rishi",
+    "Liz",
+    "Anna",
+    "Ryan",
+    "Connie",
+    "Sarah",
+    "Andy",
+    "Michael",
+    "Paul",
+    "Mason"
+]
+
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { students: students });
+});
+
+
+router.post('/search-records', (req, res, next) => {
+    var nameSubmitted = req.body.studentSearch;
+    var flag = false;
+    var msg = `You are not in the class ${nameSubmitted}`;
+    for (let student of students) {
+        if (nameSubmitted.toLowerCase() === student.toLowerCase()) {
+            msg = `You are in the class ${student}`;
+            break;
+        }
+    }
+    res.render('students', {message: msg});
+});
+
+module.exports = router;
